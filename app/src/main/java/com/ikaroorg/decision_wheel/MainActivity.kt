c93b0052af0b3a27432894d1ae6b337dbcdd0de1
kauas3228 <kauas3228@gmail.com>
2026-08-19 14:35:40 -0300

    refactor: remove a unsable imports

--- app/src/main/java/com/ikaroorg/decision_wheel/MainActivity.kt
+++ app/src/main/java/com/ikaroorg/decision_wheel/MainActivity.kt
@@ -4,19 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import com.ikaroorg.decision_wheel.navigation.AppNavigation
 import com.ikaroorg.decision_wheel.ui.theme.Decision_wheelTheme
 
